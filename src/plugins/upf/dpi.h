8bd32fa054187c72b0d5853460fbb23f85db7eae
Denys Haryachyy <garyachy@gmail.com>
2018-09-15 22:08:44 +0800

    Improved HTTP header parsing.

--- src/plugins/upf/dpi.h
+++ src/plugins/upf/dpi.h
@@ -31,12 +31,17 @@ int upf_dpi_add_multi_regex(upf_dpi_args_t * args, u32 * db_index, u8 create);
 int upf_dpi_lookup(u32 db_index, u8 * str, uint16_t length, u32 * app_index);
 int upf_dpi_remove(u32 db_index);
 
+#define MIN(x,y) (((x)<(y))?(x):(y))
+
 always_inline int
 upf_dpi_parse_ip4_packet(ip4_header_t * ip4, u32 dpi_db_id, u32 * app_index)
 {
   int tcp_payload_len = 0;
   tcp_header_t *tcp = NULL;
   u8 *http = NULL;
+  u8 *version = NULL;
+  u16 uri_length = 0;
+  int res = 0;
 
   if (ip4->protocol != IP_PROTOCOL_TCP)
     return -1;
@@ -49,16 +54,29 @@ upf_dpi_parse_ip4_packet(ip4_header_t * ip4, u32 dpi_db_id, u32 * app_index)
   if (tcp_payload_len < 8)
     return -1;
 
-  http = (u8*)tcp + sizeof(tcp_header_t);
+  http = (u8*)tcp + tcp_header_bytes(tcp);
 
-  if ((http[0] == 'G') &&
-      (http[1] == 'E') &&
-      (http[2] == 'T'))
+  if ((http[0] != 'G') ||
+      (http[1] != 'E') ||
+      (http[2] != 'T'))
     {
-      return upf_dpi_lookup(dpi_db_id, http, tcp_payload_len, app_index);
+      return -1;
     }
 
-  return -1;
+  http += sizeof("GET");
+  tcp_payload_len -= sizeof("GET");
+
+  version = (u8*)strchr((const char*)http, ' ');
+  if (version == NULL)
+    return -1;
+
+  uri_length = version - http;
+
+  res = upf_dpi_lookup(dpi_db_id, http,
+                       MIN(uri_length, tcp_payload_len),
+                       app_index);
+
+  return res;
 }
 
 #endif /* __included_upf_dpi_h__ */
